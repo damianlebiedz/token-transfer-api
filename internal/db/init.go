41936ce944673d144270ad178d51d30bf71cb887
damianlebiedz <damian.lebiedz@gmail.com>
2025-05-06 09:14:50 +0200

    Fix database connection
    Optimized the declaration of environment variables to not complicate the program with unnecessary elements at this stage. Optimized docker-compose by removing unnecessary parts, fixed the collision of production and test base ports.
    
    Resolves: #15

--- internal/db/init.go
+++ internal/db/init.go
@@ -17,17 +17,16 @@ func Init() {
 	user := os.Getenv("POSTGRES_USER")
 	pass := os.Getenv("POSTGRES_PASSWORD")
 	host := os.Getenv("POSTGRES_HOST")
-	port := os.Getenv("POSTGRES_PORT")
 	db := os.Getenv("POSTGRES_DB")
 
-	if user == "" || pass == "" || host == "" || port == "" || db == "" {
+	if user == "" || pass == "" || host == "" || db == "" {
 		log.Fatal("Missing one or more DB connection variables in .env file")
 	}
 
 	fmt.Printf("Connecting to DB with user: %s, host: %s...\n", user, host)
 
-	databaseURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, pass, host, port, db)
-	time.Sleep(2 * time.Second)
+	databaseURL := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", user, pass, host, db)
+	time.Sleep(3 * time.Second)
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
@@ -52,7 +51,7 @@ func Init() {
 			Address := "0x0000000000000000000000000000000000000000"
 			Balance := 1000000
 			DB.Create(&models.Wallet{Address: Address, Balance: Balance})
-			log.Printf("initialized wallet: %s with balance: %s", Address, Balance)
+			log.Printf("initialized wallet: %s with balance: %d", Address, Balance)
 		}
 	}
 }
